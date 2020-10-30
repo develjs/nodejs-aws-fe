a730c4ba3b4599d8200433fcd2f7ab08bf2adeaa
Alex <develjs@gmail.com>
2020-10-30 16:51:23 +0300

    use dev aws api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://6iktsnu615.execute-api.eu-west-1.amazonaws.com/dev/api/v1',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://6iktsnu615.execute-api.eu-west-1.amazonaws.com/dev/api/v1'
 };
 
 export default API_PATHS;
